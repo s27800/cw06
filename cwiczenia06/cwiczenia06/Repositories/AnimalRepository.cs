e76d73b26ab3e0c3c3a1b55b504ef32c133839c4
s27800 <s27800@pjwstk.edu.pl>
2024-04-17 20:17:05 +0200

    Finished

--- cwiczenia06/cwiczenia06/Repositories/AnimalRepository.cs
+++ cwiczenia06/cwiczenia06/Repositories/AnimalRepository.cs
@@ -77,25 +77,25 @@ public class AnimalRepository : IAnimalRepository
         //Definicja query
         using SqlCommand command1 = new SqlCommand();
         command1.Connection = connection;
-        command1.CommandText = "UPDATE Animal SET name = @name WHERE idAnimal = @idAnimal";
+        command1.CommandText = "UPDATE Animal SET Name = @name WHERE IdAnimal = @idAnimal";
         command1.Parameters.AddWithValue("name", animal.Name);
         command1.Parameters.AddWithValue("idAnimal", idAnimal);
         
         using SqlCommand command2 = new SqlCommand();
         command2.Connection = connection;
-        command2.CommandText = "UPDATE Animal SET description = @desc WHERE idAnimal = @idAnimal";
+        command2.CommandText = "UPDATE Animal SET Description = @desc WHERE IdAnimal = @idAnimal";
         command2.Parameters.AddWithValue("desc", animal.Desc);
         command2.Parameters.AddWithValue("idAnimal", idAnimal);
         
         using SqlCommand command3 = new SqlCommand();
         command3.Connection = connection;
-        command3.CommandText = "UPDATE Animal SET cat = @cat WHERE idAnimal = @idAnimal";
+        command3.CommandText = "UPDATE Animal SET Cat = @cat WHERE IdAnimal = @idAnimal";
         command3.Parameters.AddWithValue("cat", animal.Cat);
         command3.Parameters.AddWithValue("idAnimal", idAnimal);
         
         using SqlCommand command4 = new SqlCommand();
         command4.Connection = connection;
-        command4.CommandText = "UPDATE Animal SET area = @area WHERE idAnimal = @idAnimal";
+        command4.CommandText = "UPDATE Animal SET Area = @area WHERE IdAnimal = @idAnimal";
         command4.Parameters.AddWithValue("area", animal.Area);
         command4.Parameters.AddWithValue("idAnimal", idAnimal);
         
@@ -108,6 +108,17 @@ public class AnimalRepository : IAnimalRepository
 
     public void DeleteAnimal(int idAnimal)
     {
+        //Otworzenie polaczenia
+        using SqlConnection connection = new SqlConnection(_configuration.GetConnectionString("Default"));
+        connection.Open();
         
+        // Definiujemy query
+        using SqlCommand command = new SqlCommand();
+        command.Connection = connection;
+        command.CommandText = "DELETE FROM Animal WHERE IdAnimal = @idAnimal";
+        command.Parameters.AddWithValue("idAnimal", idAnimal);
+        
+        //Wykonanie commandow
+        command.ExecuteNonQuery();
     }
 }
